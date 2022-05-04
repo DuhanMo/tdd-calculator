5e39af63c95c16cdd0a0f40897f650d57563f9b3
duhanmo <devduhanmo@gmail.com>
2022-05-04 18:29:16 +0900

    feat: Calculator 덧셈, 뺄셈, 곱셈, 나눗셈 추가

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,18 @@
 public class Calculator {
 
-    public int add(int firstNumber, int secondNumber) {
+    public int plus(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
+    }
+
+    public int minus(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
     }
 }
--- src/test/java/CalculateTest.java
+++ src/test/java/CalculateTest.java
@@ -1,14 +1,61 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculateTest {
 
-    @Test
-    void sumTest() {
+    private int firstNumber;
+    private int secondNumber;
+
+    @BeforeEach
+    void numberSetup() {
+        firstNumber = 1;
+        secondNumber = 2;
+    }
+
+    @DisplayName("덧셈 테스트")
+    @ParameterizedTest()
+    @CsvSource({
+            "1, 2",
+            "3, 3",
+            "11, 32"
+    })
+    void plusTest(int firstNumber, int secondNumber) {
         Calculator calculator = new Calculator();
-        int firstNumber = 1;
-        int secondNumber = 2;
-        assertThat(calculator.add(firstNumber, secondNumber)).isEqualTo(3);
+        int expect = firstNumber + secondNumber;
+        assertThat(calculator.plus(firstNumber, secondNumber)).isEqualTo(expect);
+    }
+
+    @DisplayName("뺄셈 테스트")
+    @Test
+    void minusTest() {
+        Calculator calculator = new Calculator();
+        int expect = firstNumber - secondNumber;
+        assertThat(calculator.minus(firstNumber, secondNumber)).isEqualTo(expect);
+    }
+
+    @DisplayName("곱셈 테스트")
+    @Test
+    void multiplyTest() {
+        Calculator calculator = new Calculator();
+        int expect = firstNumber * secondNumber;
+        assertThat(calculator.multiply(firstNumber, secondNumber)).isEqualTo(expect);
+    }
+
+    @DisplayName("나눗셈 테스트")
+    @ParameterizedTest
+    @CsvSource({
+            "3, 1",
+            "4, 2",
+            "100, 10"
+    })
+    void divisionTest(int firstNumber, int secondNumber) {
+        Calculator calculator = new Calculator();
+        int expect = firstNumber / secondNumber;
+        assertThat(calculator.division(firstNumber, secondNumber)).isEqualTo(expect);
     }
 }
